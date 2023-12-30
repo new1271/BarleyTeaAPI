b3df03bc17f74a8840efc5da3565582047a439f6
Error 404 <36844888+new1271@users.noreply.github.com>
2023-12-30 18:53:31 +0800

    Edited build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,40 @@
+import java.util.*
+
 plugins {
-    `java-library`
 }
 
 version = "1.0"
 
-repositories {
-    mavenCentral()
+dependencies {
+    project(":BarleyTeaAPI")
+    project(":nms:BarleyTeaAPI-NMS-Supports")
+}
+
+val allProjects = listOf(project(":BarleyTeaAPI"), project(":nms:BarleyTeaAPI-NMS-Supports"))
+
+val publish: TaskProvider<Task> = tasks.register("publish") {
+    val layoutDir = File(project.buildDir.path, "libs")
+
+    logger.lifecycle(layoutDir.path)
+
+    allProjects.forEach {
+        val projectBuildDir = File(it.buildDir.path, "libs")
+        projectBuildDir.listFiles()?.forEach {
+            if (it.isFile) {
+                val dest = File(layoutDir.path, it.name)
+                logger.lifecycle("Copy ${it.name} into ${dest.path}")
+                it.copyTo(dest, overwrite = true)
+            }
+        }
+    }
+}
+
+tasks {
+    publish {
+        allProjects.parallelStream().map {
+            it.tasks["assemble"]
+        }.filter(Objects::nonNull).forEach {
+            dependsOn(it)
+        }
+    }
 }
