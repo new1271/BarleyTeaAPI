4b6d187f15ce70452a612b61eb152ec10384f953
Error 404 <36844888+new1271@users.noreply.github.com>
2024-05-22 02:44:56 +0800

    Added mojang mapping JAR output

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,27 +20,64 @@ val allProjects = listOf(project(":api"),
         project(":nms:v1_20_R2"),
         project(":nms:v1_20_R3"))
 
+val allJar = project.tasks.register("allJar")
+val mojangMappingJar = project.tasks.register("mojangMappingJar", Jar::class)
+
 tasks {
+    allJar {
+        dependsOn(jar, mojangMappingJar)
+    }
     jar {
+        logger.lifecycle("[jar] Building JAR")
         duplicatesStrategy = DuplicatesStrategy.WARN
         val layoutDir = File(project.buildDir.path, "libs")
         if (!layoutDir.exists())
             layoutDir.mkdir()
-        logger.lifecycle(layoutDir.path)
-        allProjects.parallelStream().map {
+        logger.lifecycle("[jar] " + File(layoutDir, archiveFileName.get()).path)
+        allProjects.stream().map {
             it.tasks["assemble"]
         }.filter(Objects::nonNull).forEach {
             dependsOn(it)
             it.doLast {
                 val projectBuildDir = File(it.project.buildDir.path, "libs")
+                val projectPath = it.project.path
+                logger.lifecycle("[jar] Include $projectPath")
                 projectBuildDir.listFiles()?.forEach {
-                    if (it.isFile && !it.name.endsWith("dev.jar")) {
-                        logger.lifecycle("Include ${it.path}")
+                    if (it.isFile && (!projectPath.startsWith(":nms:") || !it.name.endsWith("dev.jar"))) {
+                        logger.lifecycle("[jar] Include ${it.path}")
                         from(zipTree(it))
                     }
                 }
             }
         }
-
+    }
+    mojangMappingJar {
+        mustRunAfter(jar)
+        logger.lifecycle("[mojangMappingJar] Building Mojang mapped JAR")
+        duplicatesStrategy = DuplicatesStrategy.WARN
+        val filename = archiveFileName.get()
+        val lastDotIndex = filename.lastIndexOf('.')
+        if (lastDotIndex != -1)
+            archiveFileName = filename.substring(0, lastDotIndex) + "-mojang-mapped.jar"
+        val layoutDir = File(project.buildDir.path, "libs")
+        if (!layoutDir.exists())
+            layoutDir.mkdir()
+        logger.lifecycle("[mojangMappingJar] " + File(layoutDir, archiveFileName.get()).path)
+        allProjects.stream().map {
+            it.tasks["assemble"]
+        }.filter(Objects::nonNull).forEach {
+            dependsOn(it)
+            it.doLast {
+                val projectBuildDir = File(it.project.buildDir.path, "libs")
+                val projectPath = it.project.path
+                logger.lifecycle("[mojangMappingJar] Include $projectPath")
+                projectBuildDir.listFiles()?.forEach {
+                    if (it.isFile && (!projectPath.startsWith(":nms:") || it.name.endsWith("dev.jar"))) {
+                        logger.lifecycle("[mojangMappingJar] Include ${it.path}")
+                        from(zipTree(it))
+                    }
+                }
+            }
+        }
     }
 }
